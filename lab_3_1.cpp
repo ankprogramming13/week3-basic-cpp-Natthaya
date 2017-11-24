beec2b3028d74a741dc0daae8847ce09dfb89855
Unknown <natthayasofia@gmail.com>
2017-11-24 14:41:56 +0700

    ข้อที่ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Miss." << endl;
+  cout<< "Natthaya Wongchang" << endl;
+  cout<< "Nickname  Sofia" << endl;
+  cout<< "Sex female" << endl;
+  cout<< "29/12/1998" << endl;
+  cout<< "Play Gam" << endl;
+  cout<< "single" << endl;
   return 0;
 }
